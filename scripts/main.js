6dbd214245a90401173024642da9a9385cf56e31
Aaron <aaronbrodeur@Aarons-iMac.local>
2015-10-13 11:32:35 -0700

    Abstracted a bunch of the chart. Now to get it to position things by year learned & mastery level!

--- scripts/main.js
+++ scripts/main.js
@@ -11,4 +11,14 @@ $(document).ready(function(){
     $("." + filter).toggleClass("hidden");
     $(this).toggleClass("disabled");
   });
+  
+  $.getJSON("data/tools.json", function(data){
+    var items = [];
+    $.each(data.tools, function(key, val){
+      console.log(key, val);
+      items.push("<li class='tool " + val.tool_name + " " + val.tool_type + "'><span class='icon'></span><div class='info'> <a class='close-info'></a> <div class='heading clearfix'> <h2 class='left'> " + val.display_name + "<span>" + val.tool_short_description + "</span> </h2> <h2 class='right'> USED SINCE <span>" + val.used_since + "</span> </h2> </div> <div class='content'>" + val.tool_long_description + "</div> </div> </li>");
+    });
+    $(".points").html(items);
+  });
+  
 });

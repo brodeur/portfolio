bc15fc1ab814391280fe3c36328b73c7f5278266
Aaron <aaronbrodeur@Aarons-iMac.local>
2015-10-13 12:49:21 -0700

    Fixed a bunch of things, added analytics

--- scripts/main.js
+++ scripts/main.js
@@ -21,8 +21,7 @@ $(document).ready(function(){
       var years_used = val.used_since - start_year;
       var x_axis_location = 100 - ((years_used / total_years) * 100);
       var y_axis_location = 100 - val.mastery_level;
-      console.log(x_axis_location, y_axis_location);
-      items.push("<li class='tool " + val.tool_name + " " + val.tool_type + "' style='left: " + x_axis_location + "%; top: " + y_axis_location + "%;'><span class='icon'></span><div class='info'> <a class='close-info'></a> <div class='heading clearfix'> <h2 class='left'> " + val.display_name + "<span>" + val.tool_short_description + "</span> </h2> <h2 class='right'> USED SINCE <span>" + val.used_since + "</span> </h2> </div> <div class='content'>" + val.tool_long_description + "</div> </div> </li>");
+      items.push("<li class='tool " + val.tool_name + " " + val.tool_type + "' style='left: " + x_axis_location + "%; top: " + y_axis_location + "%;'><span class='icon'></span><div class='info'> <a class='close-info'></a> <div class='heading clearfix'> <h2 class='left'> " + val.display_name + "<span>" + val.tool_short_description + "</span> </h2> <h2 class='right'> USED SINCE <span>" + Math.floor(val.used_since) + "</span> </h2> </div> <div class='content'>" + val.tool_long_description + "</div> </div> </li>");
     });
     $(".points").html(items);
   });

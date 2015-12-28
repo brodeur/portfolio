83566a3456de233c9c8bdfddd07d85e2607b2462
Aaron <aaronbrodeur@Aarons-iMac.local>
2015-12-28 11:34:21 -0800

    Testing mobile touch sensitivity

--- scripts/jquery.slimscroll.js
+++ scripts/jquery.slimscroll.js
@@ -70,7 +70,7 @@
         wheelStep : 20,
 
         // scroll amount applied when user is using gestures
-        touchScrollStep : 20,
+        touchScrollStep : 30,
 
         // sets border radius
         borderRadius: '7px',
